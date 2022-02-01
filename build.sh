5ed58ccf503d716b0dce3e33693cd52e03a0a58f
Kyle Gallatin <kylegallatin@kyles-mbp.myfiosgateway.com>
2022-02-01 15:05:34 -0500

    deploy to gke

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t job-scheduler job_scheduler
+docker build -t gcr.io/serene-radius-314018/job-scheduler/job-scheduler:0.0.1 job_scheduler
 kubectl --context docker-desktop delete -f job_scheduler/templates/job_scheduler.yaml
 kubectl --context docker-desktop apply -f job_scheduler/templates/job_scheduler.yaml

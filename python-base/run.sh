fb323ca463aed5d93c298785ff10d1b81c5d4be9
Kyle Gallatin <kylegallatin@kyles-mbp-2.myfiosgateway.com>
2022-05-02 21:22:35 -0400

    finish copy script

--- python-base/run.sh
+++ python-base/run.sh
@@ -3,5 +3,6 @@ set -eux
 
 gsutil cp -r $1/* /workspace
 pip install -r requirements.txt --no-cache-dir
-exec $2
+$2
+echo "Run command finished, copying files back to $1/output"
 gsutil cp -r /workspace $1/output
